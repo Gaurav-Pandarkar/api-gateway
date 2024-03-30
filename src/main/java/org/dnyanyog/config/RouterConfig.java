d39be593f90160f8973118a8cc1ffbac69effc39
unknown <gauravpandarkar5@gmail.com>
2024-03-30 17:26:02 +0530

    feat:Implement git commit conventions
        * Implement google java formatter
        * Add pre-commit-config.yaml file

--- src/main/java/org/dnyanyog/config/RouterConfig.java
+++ src/main/java/org/dnyanyog/config/RouterConfig.java
@@ -8,11 +8,15 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RouterConfig {
 
-	@Bean
-	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-		return builder.routes()
-				.route("directory_public_route", r -> r.path("/api/v1/public/auth/**").uri("http://localhost:8081"))
-				.route("directory_route", r -> r.path("/api/v1/auth/**").uri("http://localhost:8081"))
-				.route("customer_route", r -> r.path("/api/v1/customer/**").uri("http://localhost:8082")).build();
-	}
+  @Bean
+  public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+    return builder
+        .routes()
+        .route(
+            "directory_public_route",
+            r -> r.path("/api/v1/public/auth/**").uri("http://localhost:8081"))
+        .route("directory_route", r -> r.path("/api/v1/auth/**").uri("http://localhost:8081"))
+        .route("customer_route", r -> r.path("/api/v1/customer/**").uri("http://localhost:8082"))
+        .build();
+  }
 }
