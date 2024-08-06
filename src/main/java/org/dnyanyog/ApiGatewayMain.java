3ee0a7e9c0565291e22034eba548150d3612a7de
Gaurav Pandarkar <gauravpandarkar5@dnyanyog.org>
2024-08-06 21:10:42 +0530

    feat:Implement github action on api-gateway
        * Implement api-gateway service
        * Implement github action on it

---
+++ src/main/java/org/dnyanyog/ApiGatewayMain.java
@@ -0,0 +1,15 @@
+package org.dnyanyog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ApiGatewayMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayMain.class, args);
+
+	}
+
+}
